246104ae3b8221b4d71d7e648b609ca9f078f797
carmensolano <00029613@uca.edu.sv>
2020-03-22 02:20:21 -0600

    matriz 3x3

--- Labo 1/main.cpp
+++ Labo 1/main.cpp
@@ -6,10 +6,11 @@ int main(void) {
 
 	Matrix example_matrix;
 
-	zeroes(example_matrix, 2);
+	zeroes(example_matrix, 3);
 
-	example_matrix.at(0).at(0) = 1; example_matrix.at(0).at(1) = 2;
-	example_matrix.at(1).at(0) = 3; example_matrix.at(1).at(1) = 4;
+	example_matrix.at(0).at(0) = 1; example_matrix.at(0).at(1) = 2; example_matrix.at(0).at(2) = 3;
+	example_matrix.at(1).at(0) = 4; example_matrix.at(1).at(1) = 5; example_matrix.at(1).at(2) = 6;
+	example_matrix.at(2).at(0) = 7; example_matrix.at(2).at(1) = 8; example_matrix.at(2).at(2) = 9;
 
 	showMatrix(example_matrix);
 
