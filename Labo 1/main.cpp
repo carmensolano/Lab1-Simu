66757e9e4cae26d8a5b550fd1eb9a386ab4bb73e
carmensolano <00029613@uca.edu.sv>
2020-03-22 02:16:28 -0600

    Matriz 2x2

--- Labo 1/main.cpp
+++ Labo 1/main.cpp
@@ -4,9 +4,19 @@
 
 int main(void) {
 
+	Matrix example_matrix;
+
+	zeroes(example_matrix, 2);
+
+	example_matrix.at(0).at(0) = 1; example_matrix.at(0).at(1) = 2;
+	example_matrix.at(1).at(0) = 3; example_matrix.at(1).at(1) = 4;
+
+	showMatrix(example_matrix);
+
+	/* MATRIZ DE CEROS
 	Matrix zeroes_matrix;
 	zeroes(zeroes_matrix, 5);
 	showMatrix(zeroes_matrix);
-
+	*/
 	return 0;
 }
